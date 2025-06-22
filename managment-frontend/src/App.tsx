bb9da26eda90738f11ce685fe345c8095d5ad08b
Matias Daneri <mdaneri@itba.edu.ar>
2025-06-21 22:03:16 -0300

    Moved Provider to App.tsx instead of main.tsx. Improved css

--- managment-frontend/src/App.tsx
+++ managment-frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Dashboard from './pages/Dashboard';
+import { store } from './stores';
+import { Provider } from 'react-redux';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Router>
+    <Provider store={store}>
+      <Router>
           <Routes>
             <Route path="/" element={<Dashboard />} />
           </Routes>
       </Router>
+    </Provider>
   )
 }
 
