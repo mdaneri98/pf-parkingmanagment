b0d1537da191b348559725877cc4c64abd366b9d
Matias Daneri <mdaneri@itba.edu.ar>
2025-06-22 15:30:45 -0300

    add BaseLayout template and integrate with AppInitializer for consistent navigation

--- managment-frontend/src/App.tsx
+++ managment-frontend/src/App.tsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Dashboard from './pages/Dashboard';
 import ParkingLotProfile from './pages/ParkingLotProfile';
 import { store } from './stores';
 import { Provider } from 'react-redux';
 import AppInitializer from './components/AppInitializer';
+import BaseLayout from './templates/BaseLayout';
 import './App.css'  
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Provider store={store}>
       <AppInitializer>
         <Router>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/profile" element={<ParkingLotProfile />} />
-            </Routes>
+          <BaseLayout>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/profile" element={<ParkingLotProfile />} />
+              </Routes>
+            </BaseLayout>
         </Router>
       </AppInitializer>
     </Provider>
