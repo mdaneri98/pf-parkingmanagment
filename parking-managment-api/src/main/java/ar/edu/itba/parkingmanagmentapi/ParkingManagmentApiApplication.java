28ebca54413075e95e5f0486ae09afa94e4b10c6
mdaneri98 <mdaneri98@gmail.com>
2025-07-06 12:58:17 -0300

    Added apllication config, servlet and docker compose file

---
+++ parking-managment-api/src/main/java/ar/edu/itba/parkingmanagmentapi/ParkingManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.parkingmanagmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagmentApiApplication.class, args);
+	}
+
+}
