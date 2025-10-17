43fa68fb24083e43bf65118750002365204c662c
Matias Daneri <mdaneri98@gmail.com>
2025-10-17 18:47:56 -0300

    Removed SpotCard because isnt being used. Later on, it should be moved into this component again.

--- parking-managment-web/src/features/parking/components/spots/index.ts
+++ parking-managment-web/src/features/parking/components/spots/index.ts
@@ -1,5 +1,4 @@
 export { ParkingSpotsGrid } from './ParkingSpotsGrid';
-export { SpotCard } from './SpotCard';
 export { SpotFilters } from './SpotFilters';
 export { SpotDetailModal } from './SpotDetailModal';
 export { CreateSpotModal } from './CreateSpotModal';
