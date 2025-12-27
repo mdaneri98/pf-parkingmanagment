d4712099572f7de12d100fedc9fdaacb2fb2a2e9
Matias Daneri <mdaneri98@gmail.com>
2025-12-26 21:08:39 -0300

    Configure Babel with module resolver for path aliases

--- parking-management-mobile/babel.config.js
+++ parking-management-mobile/babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@features': './src/features',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@theme': './src/theme',
+          '@api': './src/api',
+          '@store': './src/store',
+          '@constants': './src/constants',
+          '@types': './src/types',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
