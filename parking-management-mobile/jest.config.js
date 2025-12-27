6f5ea193f458d3dfc4858b3747fb2b973ac62254
Matias Daneri <mdaneri98@gmail.com>
2025-12-27 19:31:37 -0300

    Started base project of mobile android and iOs apps

--- parking-management-mobile/jest.config.js
+++ parking-management-mobile/jest.config.js
@@ -1,3 +1,37 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-vector-icons|react-native-config)/)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@theme/(.*)$': '<rootDir>/src/theme/$1',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/*.test.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
 };
